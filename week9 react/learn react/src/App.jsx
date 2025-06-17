67a8795e65f1a7ef448eabf1aeb0725c0533acf2
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-06-17 16:27:13 +0530

    useState

--- week9 react/learn react/src/App.jsx
+++ week9 react/learn react/src/App.jsx
@@ -18,10 +18,17 @@ function Counter() {
   const increaseCount=()=>{
     setcount(count+1);
   }
-
+  const decreaseCount=()=>{
+    setcount(count-1);
+  }
+  const resetCount=()=>{
+    setcount(0);
+  }
   return <div>
     <h1>{count}</h1>
     <button onClick={increaseCount}>Increase Count</button>
+    <button onClick={decreaseCount}>Decrease Count</button>
+    <button onClick={resetCount}>Reset Count</button>
   </div>
 }
 
