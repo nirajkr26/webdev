67add1c6fb33329ba0c6b9b29ba74218523ea59a
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-06-17 16:10:45 +0530

    first react app

---
+++ week9 react/learn react/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+  return <div>
+    <b>
+      hi there
+    </b>
+    {/* rendering counter */}
+    <Counter></Counter>
+  </div>
+}
+
+function Counter() {
+  return <div>
+    <h1>1</h1>
+    <button>Increase Count</button>
+  </div>
+}
+
+export default App
