89c9d3bcf328bb09f6014cf8294dce0f583ab701
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-06-18 21:36:05 +0530

    loops in ejs

--- ejs/index.js
+++ ejs/index.js
@@ -24,10 +24,12 @@ app.get("/rolldice", (req, res) => {
     });
 })
 
-app.get("/ig/:username",(req,res)=>{
-    let {username}=req.params;
-    res.render("instagram.ejs",{
-        username
+app.get("/ig/:username", (req, res) => {
+    const followers = ["jhon", "bob", "shyam", "adam"]
+    let { username } = req.params;
+    res.render("instagram.ejs", {
+        username,
+        followers
     })
 })
 
