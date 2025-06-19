02f384278ccd3759539c4af849fbf9b7bbb44bdf
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-06-19 09:01:26 +0530

    RESTful Api

--- REST/index.js
+++ REST/index.js
@@ -1,32 +1,56 @@
 const express = require("express");
 const app = express();
 const path = require("path")
+const { v4: uuidv4 } = require('uuid');
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname, "public")));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const posts = [
     {
+        "id": uuidv4(),
         "username": "niraj",
-        "content":"hello world"
+        "content": "hello world"
     },
     {
+       "id": uuidv4(),
         "username": "saurabh",
-        "content":"hello duniya"
+        "content": "hello duniya"
     },
     {
+       "id": uuidv4(),
         "username": "piyush",
-        "content":"hello guys"
+        "content": "hello guys"
     }
 
 ]
 app.get("/posts", (req, res) => {
-    res.render("index",{
+    res.render("index", {
         posts
+    })
+})
+
+app.get("/posts/new", (req, res) => {
+    res.render("new")
+})
+
+app.post("/posts", (req, res) => {
+    posts.push(
+        req.body
+    )
+    res.redirect("/posts")
+})
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post=posts.find(post=>post.id==id);
+    res.render("show",{
+        post
     })
 })
 
