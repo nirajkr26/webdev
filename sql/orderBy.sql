c7c143ba39499893bdae53d3d8902fda935dcaa2
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-06-20 09:58:55 +0530

    order by

---
+++ sql/orderBy.sql
@@ -0,0 +1,10 @@
+--to sort the data in ascending or descending order
+
+--Asc for ascending order
+--Desc for descending order
+
+select name from teacher order by age asc;
+
+select * from teacher order by name desc;
+
+SELECT * from teacher order by name;-- by default it is ascending order
