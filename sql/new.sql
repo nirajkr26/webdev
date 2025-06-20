a86cfcca7e6de93b7d208165b51490b418a74155
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-06-20 08:27:38 +0530

    creating a new table

--- sql/new.sql
+++ sql/new.sql
@@ -2,3 +2,4 @@ create DATABASE learn;  --used to create a new db
 DROP DATABASE learn;  --used to delete a db
 
 use learn;--used to select a db
+
---
+++ sql/table.sql
@@ -0,0 +1,6 @@
+create Table student(
+    roll_no INT,
+    name VARCHAR(30),
+    age INT
+); --creating a new table
+
