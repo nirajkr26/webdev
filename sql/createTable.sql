2c679639e2ee1f02f84c05ac34a91cce1e69fae0
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-06-20 09:19:34 +0530

    primary and foreign key

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -6,6 +6,15 @@ CREATE Table Teacher(
  age INT DEFAULT 18,
  subject VARCHAR(30) ,
  constraint subject_check check(subject in ('Maths','Science','English')),
+ PRIMARY KEY(id),
+ 
+);
+
+CREATE TABLE salary(
+    id INT PRIMARY KEY,
+    salary INT not NULL,
+    user_id INT,
+    Foreign Key (user_id) REFERENCES teacher(id)
 );
 
 
@@ -15,3 +24,7 @@ NOT NULL--column cannot be null
 UNIQUE--column must be unique
 default--column must have a default value
 check--column must satisfy a condition
+
+PRIMARY Key--makes a column unique and not null but used only for one
+
+Foreign Key --prevent actions that would destroy links between tables 
