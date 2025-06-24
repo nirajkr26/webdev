de88c1a0baa5c8b6a8fd11d2defea0ab113a446e
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-06-24 06:43:48 +0530

    layout-outlet in react

--- week9 react/learn react 2/src/App.jsx
+++ week9 react/learn react 2/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 
 import "./App.css";
 
@@ -8,18 +8,43 @@ function App() {
       {/* dumb way to do routing */}
       <BrowserRouter>
 
-        <Link to="/">Home</Link>||
-
-        <Link to="/neet/online-coaching-class-11">Class 11</Link>||
-        <Link to="/neet/online-coaching-class-12">Class 12</Link>
         <Routes>
-          <Route path="/neet/online-coaching-class-11" element={<Class11Program />}></Route>
-          <Route path="/neet/online-coaching-class-12" element={<Class12Program />}></Route>
-          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route path="/neet/online-coaching-class-11" element={<Class11Program />}></Route>
+            <Route path="/neet/online-coaching-class-12" element={<Class12Program />}></Route>
+            <Route path="/" element={<LandingPage />}></Route>
+            <Route path="*" element={<ErrorPage />}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
   )
+}
+
+function Layout() {
+  return <div style={{height:"100vh"}}>
+    <Link to="/">Home</Link>||
+    <Link to="/neet/online-coaching-class-11">Class 11</Link>||
+    <Link to="/neet/online-coaching-class-12">Class 12</Link>
+
+    <div style={{height:"85vh"}}>
+      <Outlet />
+    </div>
+    <div style={{backgroundColor:"white",color:"Black",height:"5vh" }}>Footer</div>
+    
+  </div>
+}
+
+function ErrorPage() {
+  const navigate = useNavigate();
+  const onClicked = () => {
+    navigate("/")
+  }
+
+  return <div>
+    <h1>Some Error occurred</h1>
+    <button onClick={onClicked}>Go to Home</button>
+  </div>
 }
 
 function LandingPage() {
@@ -36,13 +61,13 @@ function Class11Program() {
 
 function Class12Program() {
   const navigate = useNavigate();
-  const onClick = () => {
+  const onClicked = () => {
     navigate("/");
   };
 
   return <div>
     <h1>Class 12 PCB</h1>
-    <button onClick={onClick}>Back</button>
+    <button onClick={onClicked}>Back</button>
   </div>
 }
 
