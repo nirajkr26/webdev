71932e53eb44c4b2920743a5e53a96c629d469fa
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-06-26 22:03:27 +0530

    custom hooks

---
+++ week9 react/learn react 4/src/App.jsx
@@ -0,0 +1,21 @@
+
+import { useState } from 'react'
+import './App.css'
+
+//custom hook function must start with use
+//must use a hook inside a function
+
+// function useValue(){
+//   const [value,setValue]=useState(0);
+
+//   return value;
+// }
+
+function App() {
+  
+  return <div>
+    
+  </div>
+}
+
+export default App
