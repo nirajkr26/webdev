6bdee2add6f6a2c7cd842418bbe96dccbe233aee
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-06-29 16:27:21 +0530

    insert into db

--- node js with sql/index.js
+++ node js with sql/index.js
@@ -1,21 +1,38 @@
 const { faker } = require('@faker-js/faker');
-const mysql=require("mysql2");
+const mysql = require("mysql2");
 
 //to use sql from cli run --> /usr/local/mysql/bin/mysql -u root -p
 
-const connection=mysql.createConnection({
-    host:"127.0.0.1",
-    user:"root",
-    password:"",
-    database:"learn"
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "learn"
 })
 
-try{
-    connection.query("show tables",(err,res)=>{
-        if(err) throw err;
+try {
+    connection.query("show tables", (err, res) => {
+        if (err) throw err;
         console.log(res);
     })
-}catch(e){
+} catch (e) {
+    console.log(e);
+}
+
+let insertQuery = "Insert into users (id,username,email,password) values (?,?,?,?)";
+
+let users = [
+    ["123", "user1", "user@gmail.com", "123456"]
+    ["132", "user2", "user2@gmail.com", "123456"]
+    ["231", "user3", "user3@gmail.com", "123456"]
+]
+
+try {
+    connection.query(insertQuery, [users], (err, res) => {
+        if (err) throw err;
+        console.log(res);
+    })
+} catch (e) {
     console.log(e);
 }
 
