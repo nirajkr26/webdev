528504404e5a43ccc83479f3c1c38f673fd8648d
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-11-25 17:20:47 +0530

    avoiding sql injection

--- postgres/src/index.ts
+++ postgres/src/index.ts
@@ -1,14 +1,33 @@
 import { Client } from "pg";
-
+import express from "express";
 // const pgClient = new Client("can enter direct conn string here");
+const app = express();
+app.use(express.json())
 
 const pgClient2 = new Client({
     user: "neondb_owner",
-    password:"",
+    password: "",
     port: 5432,
     host: "ep-empty-union-adym5ek5-pooler.c-2.us-east-1.aws.neon.tech",
     database: "neondb",
     ssl: true
+})
+
+app.post("/todo", async (req, res) => {
+    const { title, description, done } = req.body
+    try {
+        const insertQuery = "Insert into Todo (title, description, done) values ($1, $2, $3);"
+
+        const response = await pgClient2.query(insertQuery, [title, description, done]);
+
+        res.json({
+            message: "todo added"
+        })
+    } catch (e: any) {
+        res.json({
+            message: e.message
+        })
+    }
 })
 
 const main = async () => {
@@ -21,4 +40,5 @@ const main = async () => {
 
 main();
 
+app.listen(3000)
 
