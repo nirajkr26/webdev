bcc4f142a5dad5ec761d9222792381cc2f4f0eb8
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-12-01 11:02:21 +0530

    adding database

--- nextjs/app/(auth)/signup/page.tsx
+++ nextjs/app/(auth)/signup/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import axios from "axios"
+import { useRouter } from "next/navigation";
 import { useState } from "react"
 
 export default function Signup() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-
+    const router = useRouter()
 
 
     return (
@@ -15,10 +16,12 @@ export default function Signup() {
                 <input onChange={(e)=>setUsername(e.target.value)} type="text" placeholder="username" />
                 <input onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="password" />
 
-                <button onClick={() => {
-                    axios.post("http://localhost:3000/api/v1/signup", {
+                <button onClick={async () => {
+                    await axios.post("http://localhost:3000/api/v1/signup", {
                         username, password
                     })
+
+                    router.push("/signin")
                 }}>Sign Up</button>
             </div>
 
--- nextjs/app/api/v1/signup/route.ts
+++ nextjs/app/api/v1/signup/route.ts
@@ -1,10 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
+const prismaClient = new PrismaClient();
 
 export async function POST(req: NextRequest) {
     const data = await req.json();
 
-    console.log(data)
+    await prismaClient.user.create({
+        data:{
+            username:data.username,
+            password: data.password
+        }
+    })
+
     return NextResponse.json({
         message: "You have been signed up"
     })
