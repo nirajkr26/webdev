19a1fecdac60dd2b60782213269b0fc9fa7d916e
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-12-01 11:23:43 +0530

    singleton prisma

--- nextjs/app/api/v1/signup/route.ts
+++ nextjs/app/api/v1/signup/route.ts
@@ -1,14 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient();
+import prismaClient from "../../../lib/db"
 
 export async function POST(req: NextRequest) {
     const data = await req.json();
 
     await prismaClient.user.create({
-        data:{
-            username:data.username,
+        data: {
+            username: data.username,
             password: data.password
         }
     })
